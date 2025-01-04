7a06d3db64527952f8e32d603aa85c8c7d25ce10
Antonina Kukul <kukul.antonio05@gmail.com>
2025-01-04 13:38:49 +0700

    интерация1

---
+++ interacia1.py
@@ -0,0 +1,30 @@
+import numpy as np
+import random
+
+class MatrixOperations:
+    def __init__(self):
+        self.matrix1 = None
+        self.matrix2 = None
+        self.result = None
+
+    def generate_random_matrix(self, size):
+        pass
+
+    def input_matrix(self, size):
+        pass
+
+    def display_matrix(self, matrix, name):
+        pass
+
+    def sum_matrices(self, matrix1, matrix2):
+        pass
+
+    def calculate_determinant(self, matrix):
+        pass
+
+    def main(self):
+        pass
+
+if __name__ == "__main__":
+    matrix_ops = MatrixOperations()
+    matrix_ops.main()
